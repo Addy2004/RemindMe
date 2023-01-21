74d52d89734a752f63e93fafff4a802b94327c43
Addy2004 <singhaadi2004@gmail.com>
2023-01-21 20:38:02 +0530

    Running integration test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,2 @@
 //goofy ahh unit test :D
 
-import 'package:test/test.dart';
-class Counter{
-  int value = 0;
-
-  void increment() => value++;
-
-  void decrement() => value--;
-}
-void main(){
-  test('Counter value should be incremented', () {
-   final  counter = Counter();
-
-    counter.increment();
-
-    expect(counter.value, 1);
-  });
-}
