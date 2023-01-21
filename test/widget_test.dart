b8029b10580e2bd925b16f95451877bb9322b020
Addy2004 <singhaadi2004@gmail.com>
2023-01-21 20:14:11 +0530

    Running unit tests (Attempt #2)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+//goofy ahh unit test :D
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+class Counter{
+  int value = 0;
 
-import 'package:remind_me/main.dart';
+  void increment() => value++;
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  void decrement() => value--;
+}
+void main(){
+  test('Counter value should be incremented', () {
+   final  counter = Counter();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    counter.increment();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(counter.value, 1);
   });
 }
